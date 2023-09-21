666f423e0890b1ac0c82efcba590a555587c9f0f
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-20 22:28:49 -0500

    modularizacion

--- clase-3-1/index.js
+++ clase-3-1/index.js
@@ -1,21 +1,10 @@
 import * as demos from './src/demos.js'
-import {UI} from 'ui-game-console'
-import {ui} from './src/ui.js'
-import {Player} from './src/player.js'
-import {Map} from './src/map.js'
-import {Item} from './src/item.js'
+import {Game} from './src/game.js'
 
 //demos.acceso()
 //demos.recorrido()
 //demos.indexing2to1()
 //demos.indexing1to2()
 
-
-let map = new Map()
-map.draw()
-
-let player = new Player()
-player.draw()
-
-let item = new Item()
-item.draw()
+let game = new Game()
+game.draw()
--- clase-3-1/src/game.js
+++ clase-3-1/src/game.js
@@ -1,8 +1,8 @@
-import Item from './item.js'
-import Map from './map.js'
-import Player from './player.js'
+import {Item} from './item.js'
+import {Map} from './map.js'
+import {Player} from './player.js'
 
-export default class Game {
+export class Game {
   constructor() {
     this.map = new Map()
     this.player = new Player()

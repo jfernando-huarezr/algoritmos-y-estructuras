01b0cfb6ffffeabdeb05d6ec8f000e6433818e0c
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-11-24 21:55:06 -0500

    arbol binario

---
+++ arbol-binario/index.js
@@ -0,0 +1,13 @@
+import BinaryTree from "./src/binary-tree.js"
+
+let binaryTree = new BinaryTree()
+
+//insertar nodos al arbol. Se deberia insertar un nodo debajo de otro nodo.
+//si el arbol esta vacio, por defecto el window deberia ser el root
+//si el arbol esta vacio, tambien si solo le paso un valor, debe ser el root. Si hay root, debe estar abajo del root
+
+binaryTree.insert('A')
+binaryTree.insert('B')
+binaryTree.insert('C')
+binaryTree.insert('D')
+
---
+++ arbol-binario/src/binary-tree.js
@@ -0,0 +1,97 @@
+//clase arbol
+class Node {
+    constructor(value) {
+        this.value = value,
+        this.left = null,
+        this.right = null
+    }
+}
+
+export default class BinaryTree {
+    constructor() {
+        this.root = null
+    }
+    
+    insert(value) {
+        //se crea el nuevo 
+        let newNode = new Node(value)
+        //si el arbol esta vacio, root es newNode
+        if (!this.root) {
+            this.root = newNode
+            return
+        }
+        //buscamos un nodo libre en donde insertarlo
+        let freeNode = this.searchFreeNode()
+        //si left esta ocupado, insertamos en right
+        if (freeNode.left) freeNode.right = newNode
+        //de lo contrario, insertamos en left
+        else freeNode.left = newNode
+    }
+
+    searchFreeNode() {
+        //algoritmo BFS
+        //se encola el primer elemento
+        let queue = [this.root]
+        //mientras la cola tenga elementos
+        while (queue.length > 0) {
+            //1. desencolar (dequeue -> shift)
+            let current = queue.shift()
+            //2. procesar: verificar
+            if (!this.current.left || !this.current.right) return this.current
+            //3. encolar a los hijos, si existen
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }       
+    }
+
+    print () {
+        //algoritmo BFS
+        //se encola el primer elemento
+        let queue = [this.root]
+        //mientras la cola tenga elementos
+        while (queue.length > 0) {
+            //1. desencolar (dequeue -> shift)
+            let current = queue.shift()
+            //2. procesar: imprimir
+            console.log(current.value)
+            //3. encolar a los hijos, si existen
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }       
+    }
+
+
+    remove(value) {
+        //si root es el que hay que eliminar
+        if(this.root.value === value && this.root) {
+            this.root = null
+            return
+        }
+
+        let parentNode = this.searchParent(value)
+        if (parentNode) {
+            if(parentNode.left.value === value) parentNode.left = null
+            else parentNode.right = null
+        }
+    }
+
+    searchParent(value) {
+        //algoritmo BFS
+        //se encola el primer elemento
+        let queue = [this.root]
+        //mientras la cola tenga elementos
+        while (queue.length > 0) {
+            //1. desencolar (dequeue -> shift)
+            let current = queue.shift()
+            //2. procesar: verificar si el nodo es padre de value
+            if (current.left.value === value) return current
+            if (current.right.value === value) return current
+            //3. encolar a los hijos, si existen
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }
+
+        return null
+    }
+
+}
